ff1e5058c7e503f6261fa6af1906ff5b0d0103c9
Aloxan12 <kvakvaaaak@gmail.com>
2024-10-11 13:08:43 +0300

    search

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import federation from "@originjs/vite-plugin-federation";
+// import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/vite-project/',
   plugins: [
     react(),
-    federation({
-      name: "todoList",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./TodoList": "./src/App",
-        "./components": "./src/components/Checkbox/AppCheckbox.tsx",
-      },
-      shared: ["react", "react-dom", 'mobx', 'axios', 'mobx-react'],
-    }),
+    // federation({
+    //   name: "todoList",
+    //   filename: "remoteEntry.js",
+    //   exposes: {
+    //     "./TodoList": "./src/App",
+    //     "./components": "./src/components/Checkbox/AppCheckbox.tsx",
+    //   },
+    //   shared: ["react", "react-dom", 'mobx', 'axios', 'mobx-react'],
+    // }),
   ],
   build: {
     modulePreload: false,
